87c1f42f80f5390df476c1a456635f99ba1b929d
NiladriHazra <iladrivit@gmail.com>
2025-12-18 01:29:14 +0530

    fix:build errors

--- next.config.js
+++ next.config.js
@@ -3,13 +3,7 @@ const nextConfig = {
     images: {
       domains: ['images.unsplash.com'],
     },
-    webpack(config) {
-      config.externals.push({
-        'utf-8-validate': 'commonjs utf-8-validate',
-        'bufferutil': 'commonjs bufferutil',
-      });
-      return config;
-    },
+    turbopack: {},
     env: {
       NEXT_PUBLIC_GEMINI_API_KEY: process.env.GEMINI_API_KEY,
     },
